6b0dca3db5a0c0455003366d84b32d1b87c4da75
afrizaladiestya03 <afrizalputra11@gmail.com>
2023-05-25 14:26:18 +0700

    Menghapus sidebar

--- kontakAja-app/resources/views/dashboard.blade.php
+++ kontakAja-app/resources/views/dashboard.blade.php
@@ -912,29 +912,7 @@
                 </div>
             </div>
         </header>
-        <aside class="sidebar">
-            <div class="sidebar__backdrop"></div>
-            <div class="sidebar__container">
-                <div class="sidebar__top">
-                    <div class="container container--sm">
-                        <a class="sidebar__logo" href="index.html">
-                            <div class="sidebar__logo-text">KontakAja!</div>
-                        </a>
-                    </div>
-                </div>
-                <div class="sidebar__content" data-simplebar="data-simplebar">
-                    <nav class="sidebar__nav">
-                        <ul class="sidebar__menu">
-                            <li class="sidebar__menu-item"><a class="sidebar__link active" href="index.html"
-                                    aria-expanded="true"><span class="sidebar__link-icon">
-                                        <svg class="icon-icon-dashboard">
-                                            <use xlink:href="#icon-dashboard"></use>
-                                        </svg></span><span class="sidebar__link-text">Dashboard</span></a>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </aside>
+
         <main class="page-content">
 
 
