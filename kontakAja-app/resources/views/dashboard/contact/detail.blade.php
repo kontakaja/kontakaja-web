d2a2a21c81bd3389d58265ffa2ec89e35d147e75
afrizaladiestya <afrizalputra11@gmail.com>
2023-05-30 15:16:11 +0700

    edit detail

--- kontakAja-app/resources/views/dashboard/contact/detail.blade.php
+++ kontakAja-app/resources/views/dashboard/contact/detail.blade.php
@@ -14,32 +14,32 @@
                     <img src="" alt="">
                   </div>
                   <div class="detail-item">
-                    <h1 class="contact-name">Coba Coba Aja</h1>
+                    <h1 class="contact-name">Anggoro</h1>
                     <h2 class="contact-category">Keluarga</h2>
                   </div>
               
                   <div class="button-vertical">
-                    <div>
+                    <div class="whatsapp">
                       <a class="far fa-comment-dots"></a>
-                      <h3>Pesan</h3>
+                      <h3>WhatsApp</h3>
                     </div>
                     <div>
-                        <a class="fa-regular fa-phone"></a>
+                        <a class="fa fa-phone" href="tel:#"></a>
                         <h3>Telepon</h3>
                     </div>
                     <div>
-                        <a class="fa-regular fa-video"></a>
-                        <h3>Video</h3>
+                        <a class="fa-solid fa-location-dot"></a>
+                        <h3>Maps</h3>
                     </div>
                   </div>
-                  {{-- <div class="button-horizontal">
+                  <div class="button-horizontal">
                     <div class="number">
-                        <h2>081283612300</h2>
+                          <h2>081283612300</h2>
                     </div>
-                    <div class="whatsapp">
-                        <h2>WhatsApp</h2>
+                    <div class="email">
+                        <a href="mailto:#">example@mail.com</a>
                     </div>
-                  </div> --}}
+                  </div>
                 </div>
                 <div class="delete">
                     <h3>Delete</h3>
