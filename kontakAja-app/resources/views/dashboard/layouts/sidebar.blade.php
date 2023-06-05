43cb355dc15bf75a85cb0f36715ff3a726cca831
Anggoro Rizki Aprillianto <rizkianggoro35@gmail.com>
2023-06-05 19:27:38 +0700

    merapikan code

--- kontakAja-app/resources/views/dashboard/layouts/sidebar.blade.php
+++ kontakAja-app/resources/views/dashboard/layouts/sidebar.blade.php
@@ -4,15 +4,16 @@
         <div class="sidebar__top">
             <div class="container container--sm">
                 <a class="sidebar__logo" href="index.html">
-                    <img class="sidebar__logo-icon" src="{{ asset('img/content/logotype.svg') }}" alt="Logo KontakAja!" width="44" />
+                    <img class="sidebar__logo-icon" src="{{ asset('img/content/logotype.svg') }}" alt="Logo KontakAja!"
+                        width="44" />
                 </a>
             </div>
         </div>
         <div class="sidebar__content" data-simplebar="data-simplebar">
             <nav class="sidebar__nav">
                 <ul class="sidebar__menu">
-                    <li class="sidebar__menu-item"><a class="sidebar__link" href="/dashboard"
-                            aria-expanded="true"><span class="sidebar__link-icon">
+                    <li class="sidebar__menu-item"><a class="sidebar__link" href="/dashboard" aria-expanded="true"><span
+                                class="sidebar__link-icon">
                                 <svg class="icon-icon-dashboard">
                                     <use xlink:href="#icon-dashboard"></use>
                                 </svg></span><span class="sidebar__link-text">Dashboard</span></a>
@@ -23,8 +24,8 @@
                                     <use xlink:href="#icon-contacts"></use>
                                 </svg></span><span class="sidebar__link-text">Kontak</span></a>
                     </li>
-                    <li class="sidebar__menu-item"><a class="sidebar__link" href="/dashboard/todo" aria-expanded="true"><span
-                                class="sidebar__link-icon">
+                    <li class="sidebar__menu-item"><a class="sidebar__link" href="/dashboard/todo"
+                            aria-expanded="true"><span class="sidebar__link-icon">
                                 <svg class="icon-icon-todo">
                                     <use xlink:href="#icon-todo"></use>
                                 </svg></span><span class="sidebar__link-text">Todo</span></span></a>
