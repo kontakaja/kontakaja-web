a15c8262b858b44d3f5b28620279c3d2810969ff
Anggoro Rizki Aprillianto <rizkianggoro35@gmail.com>
2023-06-16 21:15:27 +0700

    edit foto team

--- kontakAja-app/resources/views/dashboard/layouts/main.blade.php
+++ kontakAja-app/resources/views/dashboard/layouts/main.blade.php
@@ -11,10 +11,11 @@
     <meta name="description" content="KontakAja! — This is the best dashboard" />
     <meta name="msapplication-tap-highlight" content="no" />
     <meta name="mobile-web-app-capable" content="yes" />
-    <meta name="application-name" content="Arion — This is the best dashboard" />
+    <meta name="application-name" content="KontakAja! adalah aplikasi kontak yang memudahkan gaya hidup Anda." />
     <meta name="apple-mobile-web-app-title" content="KontakAja! Dashboard" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/apexcharts.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/tippy/tippy.css') }}" />
@@ -27,19 +28,7 @@
     <link rel="stylesheet" href="{{ asset('css/swiper-bundle.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style-todo.css') }}" />
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/content/favicons/apple-icon-57x57.png') }}" />
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('img/content/favicons/apple-icon-60x60.png') }}" />
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/content/favicons/apple-icon-72x72.png') }}" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/content/favicons/apple-icon-76x76.png') }}" />
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/content/favicons/apple-icon-114x114.png') }}" />
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('img/content/favicons/apple-icon-120x120.png') }}" />
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('img/content/favicons/apple-icon-144x144.png') }}" />
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/content/favicons/apple-icon-152x152.png') }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/content/favicons/apple-icon-180x180.png') }}" />
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('img/content/favicons/android-icon-192x192.png') }}" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/content/favicons/favicon-32x32.png') }}" />
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/content/favicons/favicon-96x96.png') }}" />
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/content/favicons/favicon-16x16.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
@@ -47,10 +36,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'/>
 
-    <!-- <link rel="manifest" href="img/content/favicons/manifest.json" />-->
     <meta name="msapplication-TileColor" content="#ffffff" />
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-    <meta name="theme-color" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff"/>
 </head>
 
 <body class="page-home">
