e2416d49670425faab706ef17a259256d3d1dd98
Anggoro Rizki Aprillianto <105703055+AnggoroR@users.noreply.github.com>
2023-06-18 22:17:17 +0700

    penambahan meta pada tag head

--- kontakAja-app/resources/views/auth/login.blade.php
+++ kontakAja-app/resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="dicoding:email" content="reydylanza@gmail.com">
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/styleLogin.css') }}"/>
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
